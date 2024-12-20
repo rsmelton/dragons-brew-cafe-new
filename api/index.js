807299b1eca67c1403fd96fc4920ccb179c3f7f9
Robert Melton <rsmelton@crimson.ua.edu>
2024-12-20 02:03:47 -0600

    testing some more...

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ import Item from "../models/item.model.js"
 
 const app = express();
 
-app.get("/api", async (req, res) => {
+app.get("/api/cart", async (req, res) => {
     try {
         // this grabs all Item objects from the database
         const items = await Item.find({})
