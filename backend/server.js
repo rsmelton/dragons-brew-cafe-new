076306bde355dde6fec570a7e8ece4c3efce641f
Robert Melton <rsmelton@crimson.ua.edu>
2025-01-02 20:23:31 -0600

    Still testing...

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import path from 'path'
 import { connectDB } from './config/db.js'
 import itemRoutes from './routes/item.route.js'
+import Item from '../models/item.model.js'
 
 // This allows us to use process.env.{variable name} to read from the .env file
 dotenv.config()
@@ -17,7 +18,22 @@ app.use(express.json())
 // we will need routes for CRUD Operations.
 // this appends the path /api/cart to the end of the PORT we are 
 // listening to
-app.use("/api/cart", itemRoutes) 
+// app.use("/api/cart", itemRoutes)
+app.get("/api/cart", async (req, res) => {
+    try {
+        // this grabs all Item objects from the database
+        const items = await Item.find({})
+
+        // **** newly added code ****
+        if (items === null) res.status(204).json({ success: true, data: items})
+
+        res.status(200).json({ success: true, data: items })
+    } catch (error) {
+        console.log("Error in fetching cart items:", error.message)
+        res.status(500).json({ success: false, message: "Server Error" })
+    }
+})
+
 
 // if we are in production build, go to the correct directory
 // and then send the file we want for the frontend
