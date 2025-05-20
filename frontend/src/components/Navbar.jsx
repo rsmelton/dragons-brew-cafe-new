36ac291053b9cefd7c79e905c33d2f8cf4275ec2
Robert Melton <rsmelton@crimson.ua.edu>
2025-05-20 02:27:42 -0500

    More navbar experimenting

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,35 +1,75 @@
 import React from 'react'
-import { Flex, Text, Image, Box } from '@chakra-ui/react'
+import '../assets/navbar.css'
+import { Flex, Text, Image, Box, position } from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
 import dragonsBrewCafeLogo from '../images/dragonsBrewLogoImage.jpg'
 
 const Navbar = () => {
+
     return (
-        <Flex 
-            bgColor={'black'}
-            color={'white'}
-            height={20} 
-            alignItems={'center'} 
-            justifyContent={'flex-end'} 
-            gap={{base: '1rem', md: '2rem', lg: '3rem'}} 
-            flexDirection={'row'} paddingRight={5}
-        >
-            <Text 
-                fontSize={{base: '20px', md: '25px', lg: '25px'}} 
-            >
-                <Link to={"/home"}>Home</Link>
-            </Text>
-            <Text 
-                fontSize={{base: '20px', md: '25px', lg: '25px'}} 
-            >
-                <Link to={"/menu"}>Menu</Link>
-            </Text>  
-            <Text 
-                fontSize={{base: '20px', md: '25px', lg: '25px'}} 
-            >
-                <Link to={"/cart"}>Cart</Link>
-            </Text>         
-        </Flex>
+        <>
+            <div className='navbar'>
+                <a href="/home">Home</a>
+                <a href="/menu">Menu</a>
+                <a href="/cart">Cart</a>
+            </div>
+            <div className='main'>
+                <h1>Fixed Top Menu</h1>
+                <h2>Scroll this page to see the effect</h2>
+                <h2>The navigation bar will stay at the top of the page while scrolling</h2>
+                <p>Some text some text some text some text..</p>
+                <p>Some text some text some text some text..</p>
+                <p>Some text some text some text some text..</p>
+                <p>Some text some text some text some text..</p>
+                <p>Some text some text some text some text..</p>
+                <p>Some text some text some text some text..</p>
+                <p>Some text some text some text some text..</p>
+                <p>Some text some text some text some text..</p>
+                <p>Some text some text some text some text..</p>
+                <p>Some text some text some text some text..</p>
+                <p>Some text some text some text some text..</p>
+                <p>Some text some text some text some text..</p>
+                <p>Some text some text some text some text..</p>
+                <p>Some text some text some text some text..</p>
+                <p>Some text some text some text some text..</p>
+                <p>Some text some text some text some text..</p>
+                <p>Some text some text some text some text..</p>
+                <p>Some text some text some text some text..</p>
+                <p>Some text some text some text some text..</p>
+                <p>Some text some text some text some text..</p>
+            </div>
+        </>
+
+
+
+        // <Flex 
+        //     height={20} 
+        //     alignItems={'center'} 
+        //     justifyContent={'flex-end'} 
+        //     gap={'1rem'} 
+        //     flexDirection={'row'} paddingRight={5}
+        // >
+        //     <Text 
+        //         fontSize={{base: '20px', md: '25px', lg: '25px'}} 
+        //     >
+        //         <Link to={"/home"}>Home</Link>
+        //     </Text>
+        //     <Text 
+        //         fontSize={{base: '20px', md: '25px', lg: '25px'}} 
+        //     >
+        //         <Link to={"/menu"}>Menu</Link>
+        //     </Text>  
+        //     <Text 
+        //         fontSize={{base: '20px', md: '25px', lg: '25px'}} 
+        //     >
+        //         <Link to={"/cart"}>Cart</Link>
+        //     </Text>         
+        // </Flex>
+
+
+
+
+
         // <Box className='primary_font tan_font bold' paddingTop={3} bgColor={'#3E3831'}>
         //     <Flex 
         //         height={20} 
