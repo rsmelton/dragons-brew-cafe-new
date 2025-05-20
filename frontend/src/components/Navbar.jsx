ceceadf720d792cf8beb73b6a99eadb89a8718c1
Robert Melton <rsmelton@crimson.ua.edu>
2025-05-20 01:36:55 -0500

    Experimenting with the navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,40 +5,64 @@ import dragonsBrewCafeLogo from '../images/dragonsBrewLogoImage.jpg'
 
 const Navbar = () => {
     return (
-        <Box className='primary_font tan_font bold' paddingTop={3} bgColor={'#3E3831'}>
-            <Flex 
-                height={20} 
-                alignItems={'center'} 
-                justifyContent={'center'} 
-                gap={{base: '1rem', md: '2rem', lg: '3rem'}} 
-                flexDirection={'row'} paddingLeft={10}
+        <Flex 
+            bgColor={'white'}
+            height={20} 
+            alignItems={'center'} 
+            justifyContent={'flex-end'} 
+            gap={{base: '1rem', md: '2rem', lg: '3rem'}} 
+            flexDirection={'row'} paddingRight={5}
+        >
+            <Text 
+                fontSize={{base: '20px', md: '25px', lg: '25px'}} 
             >
-                <Text 
-                    fontSize={{base: '20px', md: '25px', lg: '25px'}} 
-                    _hover={{ color: '#3EA56C' }}
-                >
-                    <Link to={"/home"}>Home</Link>
-                </Text>
-                <Image 
-                    borderRadius={'50%'} 
-                    boxSize={'95px'} 
-                    src={dragonsBrewCafeLogo} 
-                    alt='Dragons Brew Cafe' 
-                />
-                <Text 
-                    fontSize={{base: '20px', md: '25px', lg: '25px'}} 
-                    _hover={{ color: '#3EA56C' }}
-                >
-                    <Link to={"/menu"}>Menu</Link>
-                </Text>  
-                <Text 
-                    fontSize={{base: '20px', md: '25px', lg: '25px'}} 
-                    _hover={{ color: '#3EA56C' }}
-                >
-                    <Link to={"/cart"}>Cart</Link>
-                </Text>         
-            </Flex>
-        </Box>
+                <Link to={"/home"}>Home</Link>
+            </Text>
+            <Text 
+                fontSize={{base: '20px', md: '25px', lg: '25px'}} 
+            >
+                <Link to={"/menu"}>Menu</Link>
+            </Text>  
+            <Text 
+                fontSize={{base: '20px', md: '25px', lg: '25px'}} 
+            >
+                <Link to={"/cart"}>Cart</Link>
+            </Text>         
+        </Flex>
+        // <Box className='primary_font tan_font bold' paddingTop={3} bgColor={'#3E3831'}>
+        //     <Flex 
+        //         height={20} 
+        //         alignItems={'center'} 
+        //         justifyContent={'center'} 
+        //         gap={{base: '1rem', md: '2rem', lg: '3rem'}} 
+        //         flexDirection={'row'} paddingLeft={10}
+        //     >
+        //         <Text 
+        //             fontSize={{base: '20px', md: '25px', lg: '25px'}} 
+        //             _hover={{ color: '#3EA56C' }}
+        //         >
+        //             <Link to={"/home"}>Home</Link>
+        //         </Text>
+        //         <Image 
+        //             borderRadius={'50%'} 
+        //             boxSize={'95px'} 
+        //             src={dragonsBrewCafeLogo} 
+        //             alt='Dragons Brew Cafe' 
+        //         />
+        //         <Text 
+        //             fontSize={{base: '20px', md: '25px', lg: '25px'}} 
+        //             _hover={{ color: '#3EA56C' }}
+        //         >
+        //             <Link to={"/menu"}>Menu</Link>
+        //         </Text>  
+        //         <Text 
+        //             fontSize={{base: '20px', md: '25px', lg: '25px'}} 
+        //             _hover={{ color: '#3EA56C' }}
+        //         >
+        //             <Link to={"/cart"}>Cart</Link>
+        //         </Text>         
+        //     </Flex>
+        // </Box>
     )
 }
 
