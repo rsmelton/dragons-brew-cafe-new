47c7387b5ff01483d30a75eb03e1c7fbf596ee56
Robert Melton <rsmelton@crimson.ua.edu>
2025-05-23 18:46:47 -0500

    Moved App.jsx code into main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,13 +2,34 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { ChakraProvider } from '@chakra-ui/react'
-import App from './App.jsx'
+import { Box } from "@chakra-ui/react"
+import { Routes, Route } from "react-router-dom"
+import HomePage from "./pages/HomePage.jsx"
+import MenuPage from "./pages/MenuPage.jsx"
+import CartPage from "./pages/CartPage.jsx"
+import './assets/utils.css'
+import './assets/navbar.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ChakraProvider>
       <BrowserRouter>
-        <App />
+        {/* <App /> */}
+        <Box display={'flex'} flexDirection={'column'} height={'100%'}>
+          <nav className='navbar'>
+            <a href="/cart">Cart</a>
+            <a href="/menu">Menu</a>
+            <a href="/home">Home</a>
+          </nav>
+          <div className="scrollable-page">
+            <Routes>
+              <Route path='/' element={<HomePage />} />
+              <Route path='/home' element={<HomePage />} />
+              <Route path='/menu' element={<MenuPage />} />
+              <Route path='/cart' element={<CartPage />} />
+            </Routes>
+          </div>
+        </Box>
       </BrowserRouter>
     </ChakraProvider>
   </StrictMode>,
