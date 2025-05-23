4e38da853ada9913c72a2f72a5408c2ca4c239be
Robert Melton <rsmelton@crimson.ua.edu>
2025-05-22 20:34:48 -0500

    Added brackets for uniformity

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -6,7 +6,7 @@ import welcomeScreenImg from '../images/welcomeScreen.jpg'
 const HomePage = () => {
 
   return (
-    <div className='scrollable-page'>
+    <div className={'scrollable-page'}>
 
       {/* <Flex justifyContent={'center'} padding={10}>
         <video autoplay muted loop id="myVideo">
@@ -15,7 +15,7 @@ const HomePage = () => {
       </Flex> */}
       
       <Flex
-        className='primary-font'
+        className={'primary-font'}
         alignItems={'flex-start'}
         flexDirection={'column'}
         color={'white'}
@@ -23,20 +23,20 @@ const HomePage = () => {
         gap={'1.5rem'}
       >
         <Box>
-          <Text className='bold' fontSize={'2rem'}>About</Text>
+          <Text className={'bold'} fontSize={'2rem'}>About</Text>
           <Text paddingBottom={'1rem'}>The ambiance is cozy yet enigmatic, with soft instrumental music playing in the background, creating a serene atmosphere perfect for contemplation or conversation. The menu boasts a variety of unique blends and brews inspired by different dragon lore from around the world. You can savor a "Dragons Fire Latte", a bold and intense latte with a spicy kick, or perhaps indulge in a "Frost Dragon Latte", a creamy concoction infused with mint and white chocolate.</Text>
           <Text paddingBottom={'1rem'}>The baristas, clad in aprons adorned with dragon motifs, are friendly and knowledgeable, ready to guide you through the menu and recommend the perfect drink to suit your mood. Patrons are often seen relaxing with their laptops or books, enjoying the free Wi-Fi and the comforting warmth of their dragon-emblazoned ceramic mugs.</Text>
           <Text>Dragons Brew isn't just a coffee shop; it's a sanctuary where myth and reality blend seamlessly, where every sip transports you on a journey through legends and fantasies. Whether you're seeking a quick caffeine fix or a tranquil retreat from the urban hustle, Dragons Brew promises an unforgettable experience steeped in both magic and the art of coffee.</Text>
         </Box>
         <Box>
-          <Text className='bold' fontSize={'2rem'}>Contact</Text>
+          <Text className={'bold'} fontSize={'2rem'}>Contact</Text>
           <Text>PHONE NUMBER: (256)-123-4567</Text>
           <Text>LOCATION: TBA</Text>
           <Text>HOURS: 7:00 am - 4:00 pm</Text>
           <Text>Everyday</Text>
         </Box>
         <Box>
-          <Text className='bold' fontSize={'2rem'}>Reviews</Text>
+          <Text className={'bold'} fontSize={'2rem'}>Reviews</Text>
           <Text>Name: Robert Melton</Text>
           <Text>Review: ⭐️⭐️⭐️⭐️⭐️</Text>
           <Text paddingBottom={'1rem'}>Dragons Brew is my go-to spot for a perfect cup of coffee! The ambiance is cozy with just the right touch of dragon-themed decor that adds a whimsical charm. The baristas are friendly and knowledgeable, always recommending new blends to try. Their Dragon's Fire Latte is my favorite; it's rich and smooth, perfect for starting my day right!</Text>
