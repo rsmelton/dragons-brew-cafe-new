c42a6d38f087af94fdf65df7187f076eaf60b4fc
Robert Melton <rsmelton@crimson.ua.edu>
2025-05-28 20:39:39 -0500

    Updated menu page to now fetch from our new menuItems collection for cleaner, dynamic displaying of the menuItem component

--- frontend/src/pages/MenuPage.jsx
+++ frontend/src/pages/MenuPage.jsx
@@ -1,78 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Grid } from '@chakra-ui/react'
-import dragonsFireLatteImg from '../images/dragonsFireLatte.png'
-import enchatedEspressoImg from '../images/enchantedEspresso.png'
-import frostDragonLatteImg from '../images/frostDragonLatte.png'
-import mysticMatchaImg from '../images/mysticMatcha.png'
-import phoenixFrappuccinoImg from '../images/phoenixFrappuccino.png'
-import sorcerersSmoresMochaImg from '../images/sorcerersSmoresMocha.png'
-import unicornColdBrewImg from '../images/unicornColdBrew.png'
-import wizardsWhiteChocolateMochaImg from '../images/wizardsWhiteChocolateMocha.png'
+import { useMenuItemStore } from '../store/menuItem.store.js'
 import MenuItem from '../components/MenuItem'
 import '../assets/styles.css'
 
 const MenuPage = () => { 
+
+  const { menuItems, fetchMenuItems } = useMenuItemStore()
+
+  useEffect(() => { fetchMenuItems() }, [])
+
   return (
     <Grid 
       templateColumns={{base: '1fr', md: '1fr 1fr', lg: '1fr 1fr 1fr'}}
       templateRows={'1fr'}
     >
-      <MenuItem 
-        image={dragonsFireLatteImg} 
-        name={"Dragon's Fire Latte"} 
-        price={"7.95"}
-        nameAndPrice={"Dragon's Fire Latte - $7.95"} 
-        description={"A spicy twist on a classic latte with a hint of cinnamon and cayenne pepper, topped with a good amount of whipped cream"} 
-      />
-      <MenuItem 
-        image={enchatedEspressoImg} 
-        name={"Enchanted Espresso"} 
-        price={"4.95"}
-        nameAndPrice={"Enchanted Espresso - $4.95"} 
-        description={"A double shot of our finest espresso, served with a twist of lemon and a sprinkling of enchanted edible glitter upon request"} 
-      />
-      <MenuItem 
-        image={frostDragonLatteImg} 
-        name={"Frost Dragon Latte"} 
-        price={"6.95"}
-        nameAndPrice={"Frost Dragon Latte - $6.95"} 
-        description={"A freezing cold latte with mint and white chocolate with some whipped cream on top and a drizzle of caramel"} 
-      />
-      <MenuItem 
-        image={mysticMatchaImg} 
-        name={"Mystic Matcha"} 
-        price={"7.99"}
-        nameAndPrice={"Mystic Matcha - $7.99"} 
-        description={"A vibrant green matcha latte with a touch of vanilla and honey with whipped cream on top, served with a side of mochi"} 
-      />
-      <MenuItem 
-        image={phoenixFrappuccinoImg} 
-        name={"Phoenix Frappuccino"} 
-        price={"7.95"}
-        nameAndPrice={"Phoenix Frappuccino - $7.95"} 
-        description={"A cool, blended drink with flavors of mango, raspberry, and a hint of chili, topped with whipped cream and a fiery chocolate drizzle"} 
-      />
-      <MenuItem 
-        image={sorcerersSmoresMochaImg} 
-        name={"Sorcerers Smores Mocha"} 
-        price={"8.95"}
-        nameAndPrice={"Sorcerers Smores Mocha - $8.95"} 
-        description={"A decadent mocha with toasted marshmallow, graham cracker crumbles, and chocolate drizzle"} 
-      />
-      <MenuItem 
-        image={unicornColdBrewImg} 
-        name={"Unicorn Cold Brew"} 
-        price={"3.95"}
-        nameAndPrice={"Unicorn Cold Brew - $3.95"} 
-        description={"A magical cold brew coffee infused with lavender and vanilla, served with a swirl of pastel-colored cream on the top"} 
-      />
-      <MenuItem 
-        image={wizardsWhiteChocolateMochaImg} 
-        name={"Wizards White Chocolate Mocha"} 
-        price={"6.95"}
-        nameAndPrice={"Wizards White Chocolate Mocha - $6.95"} 
-        description={"A creamy white chocolate mocha mixed with a hazelnut mocha topped with whipped cream and a drizzle of caramel and chocolate"} 
-      />
+      {menuItems.map((menuItem) => {
+        return <MenuItem 
+          name={menuItem.name}
+          price={menuItem.price}   
+          description={menuItem.description} 
+          image={menuItem.imageURLString}
+        />
+      })}
     </Grid>
   )
 }
