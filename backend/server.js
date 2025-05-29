067dd3d7573a3c48bff6b659adf93fc559ad7af6
Robert Melton <rsmelton@crimson.ua.edu>
2025-05-28 20:35:14 -0500

    Updated server file with new middlewarw for the menu item routes as well as updated the item routes to now represent the cartItem routes

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,8 @@ import express from 'express'
 import dotenv from 'dotenv'
 import path from 'path'
 import { connectDB } from './config/db.js'
-import itemRoutes from './routes/item.route.js'
-import Item from '../models/item.model.js'
+import cartItemRoutes from './routes/cartItem.route.js'
+import menuItemRoutes from './routes/menuItem.route.js'
 
 // This allows us to use process.env.{variable name} to read from the .env file
 dotenv.config()
@@ -15,25 +15,10 @@ const __dirname = path.resolve()
 
 app.use(express.json())
 
-// we will need routes for CRUD Operations.
-// this appends the path /api/cart to the end of the PORT we are 
-// listening to
-app.use("/api/cart", itemRoutes)
-// app.get("/api/cart", async (req, res) => {
-//     try {
-//         // this grabs all Item objects from the database
-//         const items = await Item.find({})
-
-//         // **** newly added code ****
-//         if (items === null) res.status(204).json({ success: true, data: items})
-
-//         res.status(200).json({ success: true, data: items })
-//     } catch (error) {
-//         console.log("Error in fetching cart items:", error.message)
-//         res.status(500).json({ success: false, message: "Server Error" })
-//     }
-// })
-
+// This is where the frontend and backend meet
+// Middleware for APIs
+app.use("/api/cart", cartItemRoutes)
+app.use("/api/menu", menuItemRoutes)
 
 // if we are in production build, go to the correct directory
 // and then send the file we want for the frontend
