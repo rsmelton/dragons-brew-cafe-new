459ef51684c584946c5fd11729d07095371aeefb
Robert Melton <rsmelton@crimson.ua.edu>
2025-06-02 20:33:59 -0500

    Moved away from zustand, moved away from useToast, now using our new context wrapper, better conditional rendering, and now using a normal table instead of using the chakra table.

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -1,79 +1,55 @@
-import React, { useEffect } from 'react'
-import { Flex, Text, Table, Thead, Tbody, Tr, Th, TableContainer, Button, useToast } from '@chakra-ui/react'
+import React, { useEffect, useState } from 'react'
+import { useCart } from '../context/CartContext.jsx'
+import { Flex, Text, Button } from '@chakra-ui/react'
 import { Link } from "react-router-dom"
-import { useCartItemStore } from '../store/cartItem.store.js'
 import CartItem from '../components/CartItem.jsx'
 import '../assets/styles.css'
 
 const CartPage = () => {
 
-  const { cartItems, fetchCartItems, deleteCartItems } = useCartItemStore()
+  const {cartItems, menuItems, fetchCartItems, 
+         fetchMenuItems, handleDeleteAllCartItems, getCartItemID, 
+         getCartItemQuantity, getMenuItemName, getMenuItemPrice} = useCart()
+         
+  const [menuItemMap, setMenuItemMap] = useState({})
 
-  useEffect(() => { fetchCartItems() }, [cartItems])
+  useEffect(() => { fetchCartItems() }, [])
 
-  const toast = useToast()
+  useEffect(() => { fetchMenuItems() }, [])
+
+  useEffect(() => {
+    // This ensures we don't access the menuItems array before its populated
+    if (menuItems.length === 0) return
+
+    // Here we are converting our menuItems array to a Map so we can have constant lookup time
+    // instead of mapping over the cartItems and then having to find the menuItem every time
+    // using the cartItem._id
+    const menuMap = Object.fromEntries(menuItems.map(menuItem => [menuItem._id, menuItem]))
+    setMenuItemMap(menuMap)
+  }, [menuItems])
 
   const handleFindTotalPrice = (cartItems) => {
     let totalPrice = 0
 
     cartItems.map((cartItem) => {
-      totalPrice += cartItem.price
+      const cartItemQuantity = getCartItemQuantity(cartItem)
+      const menuItem = menuItemMap[getCartItemID(cartItem)]
+      const menuItemPrice = getMenuItemPrice(menuItem)
+      totalPrice += menuItemPrice * cartItemQuantity
     })
 
     return totalPrice.toFixed(2)
   }
 
-  const handleDeleteAllCartItems = async () => {
-    // delete all items from database to show that the user "purchased" their items
-    const {success, message} = await deleteCartItems()
-
-    if (success === false) {
-      toast({
-        title: "Error",
-        description: "Failed to purchase",
-        status: "error",
-        isClosable: true
-      })
-    } else {
-      toast({
-        title: "Success",
-        description: message,
-        status: "success",
-        isClosable: true
-      })
-    }
-  }
-
   return (
     <div>
-      <Text 
-        // paddingTop={'2rem'}
-        // paddingBottom={'2rem'}
+      {/* <Text 
         fontSize={'1.5rem'} 
         bgClip={'text'}
         color={'white'}
       >
         Cart
-      </Text>
-
-      {cartItems.length > 0 && (
-        <>
-          <table className='cart-table'>
-            {cartItems.map((cartItem, index) => {
-              return <CartItem key={cartItem._id} cartItem={cartItem} />
-            })}
-          </table>
-          <Flex
-            justifyContent={'center'}
-            alignItems={'center'}
-            flexDir={'column'}
-            gap={'1rem'}
-          >
-            <Text color={'white'} fontSize={{base: '1rem', md: '1.25rem'}}>Your total is ${handleFindTotalPrice(cartItems)}. Click below to purchase.</Text>
-            <Button bgColor={'white'} color={'black'} onClick={handleDeleteAllCartItems}>Purchase</Button>
-          </Flex>
-        </>
-      )}
+      </Text> */}
 
       {cartItems.length === 0 && (
         <Flex
@@ -86,6 +62,43 @@ const CartPage = () => {
           <Text color={'white'} textAlign={'center'}>Please click the button below to navigate to the menu to start adding items to your cart.</Text>
           <Link to={'/menu'}><Button bgColor={'white'} color={'black'}>Go to menu</Button></Link>
         </Flex>
+      )}
+
+      {/* We can display a loading message when there are items in the cart, 
+          the menuItem map has yet to be populated */}
+      {cartItems.length > 0 && Object.keys(menuItemMap).length === 0 && (
+        <Flex
+          justifyContent={'center'}
+          alignItems={'center'}
+          flexDir={'row'}
+        >
+          <Text color={'white'} fontSize={'1.5rem'}>Loading cart...</Text>
+        </Flex>
+      )}
+
+      {/* We are ensuring that we have items in the cart and that the menuItemMap has items
+          to access otherwise we would be accessing something that is undefined */}
+      {cartItems.length > 0 && Object.keys(menuItemMap).length !== 0 && (
+        <>
+          <table className='cart-table'>
+            {cartItems.map((cartItem) => {
+              const menuItem = menuItemMap[getCartItemID(cartItem)]
+              return <CartItem cartItemID={getCartItemID(cartItem)}
+                               cartItemQuantity={getCartItemQuantity(cartItem)} 
+                               menuItemName={getMenuItemName(menuItem)} 
+                               menuItemPrice={getMenuItemPrice(menuItem)} />
+            })}
+          </table>
+          <Flex
+            justifyContent={'center'}
+            alignItems={'center'}
+            flexDir={'column'}
+            gap={'1rem'}
+          >
+            <Text color={'white'} fontSize={{base: '1rem', md: '1.25rem'}}>Your total is ${handleFindTotalPrice(cartItems)}. Click below to purchase.</Text>
+            <Button bgColor={'white'} color={'black'} onClick={handleDeleteAllCartItems}>Purchase</Button>
+          </Flex>
+        </>
       )}
     </div>
   )
