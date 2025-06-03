743b00733d062d5faf633bfacd724417e0a67d2e
Robert Melton <rsmelton@crimson.ua.edu>
2025-06-02 20:26:53 -0500

    Cleaned up file a bit and wrapped our app with our new context

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,8 @@
 import { StrictMode, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { ChakraProvider } from '@chakra-ui/react'
-import { Box } from "@chakra-ui/react"
-import { Routes, Route } from "react-router-dom"
-import { useCartItemStore } from './store/cartItem.store.js'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { ChakraProvider, Box } from '@chakra-ui/react'
+import { CartProvider, useCart } from './context/CartContext.jsx'
 import HomePage from "./pages/HomePage.jsx"
 import AboutPage from './pages/AboutPage.jsx'
 import ContactPage from './pages/ContactPage.jsx'
@@ -13,61 +11,54 @@ import MenuPage from "./pages/MenuPage.jsx"
 import CartPage from "./pages/CartPage.jsx"
 import './assets/styles.css'
 
-/*
-  The items in the db are appearing on the cart page because we are using a useEffect hook to fetch the items.
-  However, we don't want to have to do that every time the user clicks on one of the nav links
-  Maybe we can achieve this by tracking some piece of state that tells us the number of items in the cart
+const CartTotalQuantity = () => {
+  const {cartTotalQuantity, fetchCartItems} = useCart()
 
-*/
-
-
-const CartItemCount = () => {
-
-  const { cartItems, fetchCartItems } = useCartItemStore()
-
-  useEffect(() => { fetchCartItems() }, [cartItems])
+  useEffect(() => { fetchCartItems() }, [])
 
   return (
-    <Box color={'white'}>{cartItems.length}</Box>
+    <Box color={'white'}>{cartTotalQuantity}</Box>
   )
 }
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ChakraProvider>
-      <BrowserRouter>
-        <Box display={'flex'} flexDirection={'column'}>
-          <nav className='navbar'>
-            <a href="/home">Home</a>
-            <a href="/about">About</a>
-            <a href="/menu">Menu</a>
-            <a href="/reviews">Reviews</a>
-            <a href="/contact">Contact</a>
-            <Box display={'flex'} flexDirection={'column'} alignItems={'center'}>
-              <CartItemCount />
-              <a href="/cart">
-                <img className='cart-icon' src={'/images/cart-icon.png'} alt="Cart icon" />
-              </a>
+      <CartProvider>
+        <BrowserRouter>
+          <Box display={'flex'} flexDirection={'column'}>
+            <nav className='navbar'>
+              <a href="/home">Home</a>
+              <a href="/about">About</a>
+              <a href="/menu">Menu</a>
+              <a href="/reviews">Reviews</a>
+              <a href="/contact">Contact</a>
+              <Box display={'flex'} flexDirection={'column'} alignItems={'center'}>
+                <CartTotalQuantity />
+                <a href="/cart">
+                  <img className='cart-icon' src={'/images/cart-icon.png'} alt="Cart icon" />
+                </a>
+              </Box>
+            </nav>
+            <Box display={'flex'} flexDirection={'row'} justifyContent={'flex-start'}>
+              <div className='page-title bold'>
+                Dragon's Brew Cafe
+              </div>
             </Box>
-          </nav>
-          <Box display={'flex'} flexDirection={'row'} justifyContent={'flex-start'}>
-            <div className='page-title bold'>
-              Dragon's Brew Cafe
+            <div className="scrollable-page">
+              <Routes>
+                <Route path='/' element={<HomePage />} />
+                <Route path='/home' element={<HomePage />} />
+                <Route path='/about' element={<AboutPage />} />
+                <Route path='/contact' element={<ContactPage />} />
+                <Route path='/reviews' element={<ReviewsPage />} />
+                <Route path='/menu' element={<MenuPage />} />
+                <Route path='/cart' element={<CartPage />} />
+              </Routes>
             </div>
           </Box>
-          <div className="scrollable-page">
-            <Routes>
-              <Route path='/' element={<HomePage />} />
-              <Route path='/home' element={<HomePage />} />
-              <Route path='/about' element={<AboutPage />} />
-              <Route path='/contact' element={<ContactPage />} />
-              <Route path='/reviews' element={<ReviewsPage />} />
-              <Route path='/menu' element={<MenuPage />} />
-              <Route path='/cart' element={<CartPage />} />
-            </Routes>
-          </div>
-        </Box>
-      </BrowserRouter>
+        </BrowserRouter>
+      </CartProvider>
     </ChakraProvider>
   </StrictMode>,
 )
