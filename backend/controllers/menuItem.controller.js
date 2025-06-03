f2758d35218c918ea0fca51d72af4dc3f52971d5
Robert Melton <rsmelton@crimson.ua.edu>
2025-06-02 20:32:11 -0500

    Changed model name from MenuItem to MenuItemModel as that is better practice.

--- backend/controllers/menuItem.controller.js
+++ backend/controllers/menuItem.controller.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
-import MenuItem from "../../models/menuItem.model.js";
+import MenuItemModel from "../../models/menuItem.model.js";
 
 export const getMenuItems = async (req, res) => {
     try {
         // this grabs all MenuItem objects from the database
-        const menuItems = await MenuItem.find({})
+        const menuItems = await MenuItemModel.find({})
 
         res.status(200).json({ success: true, data: menuItems })
     } catch (error) {
