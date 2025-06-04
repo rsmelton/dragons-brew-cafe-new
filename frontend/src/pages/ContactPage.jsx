939da8dd4ddf9a59600032490f75d1f957333d8b
Robert Melton <rsmelton@crimson.ua.edu>
2025-06-03 21:31:13 -0500

    Took out color prop

--- frontend/src/pages/ContactPage.jsx
+++ frontend/src/pages/ContactPage.jsx
@@ -3,7 +3,7 @@ import { Box, Text } from '@chakra-ui/react'
 
 const ContactPage = () => {
   return (
-    <Box color={'white'}>
+    <Box>
         <Text>(123)-456-7890</Text>
         <Text>Open every day from 7:00 am - 4:00 pm</Text>
         <Text></Text>
