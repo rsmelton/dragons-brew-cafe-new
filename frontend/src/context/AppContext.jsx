cbcc92f59784aea266806c578be0595eabfd1ad0
Robert Melton <rsmelton@crimson.ua.edu>
2025-06-06 03:20:24 -0500

    Request methods now are a little more clear as to what data is being set. Added two new methods: doesMenuItemMatchCartItem as well as handleFindTotalPriceOfCart and got rid of getCartItemID accessor as we don't need it -- Changes made in AppContext.jsx

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -7,50 +7,85 @@ export const AppProvider = ({ children }) => {
     const [menuItems, setMenuItems] = useState([])
     const [cartTotalQuantity, setCartTotalQuantity] = useState(0)
 
-    // Functions that make a request to the backend
+    // Functions that make a request to the backend: fetchCartItems down to handleDeleteAllCartItems
     const fetchCartItems = async () => {
         await fetch("/api/cart")
             .then((res) => res.json())
-            .then((data) => setCartItems(data.data))
+            .then((data) => {
+                const cartItemsData = data.data
+                setCartItems(cartItemsData)
+            })
     }
 
     const fetchMenuItems = async () => {
         await fetch("/api/menu")
             .then((res) => res.json())
-            .then((data) => setMenuItems(data.data))
+            .then((data) => {
+                const menuItemsData = data.data
+                setMenuItems(menuItemsData)
+            })
     }
 
     const handleAddToCart = async (menuItemID) => {
         await fetch(`/api/cart/${menuItemID}`, {
             method: "POST",
         }).then((res) => res.json())
-          .then((data) => setCartItems(data.data))
+          .then((data) => {
+              const cartItemsData = data.data
+              setCartItems(cartItemsData)
+          })
     }
 
-    const handleModifyCartItemQuantity = async (cartItemID, modifyQuantityBy) => {
-        await fetch(`/api/cart/${cartItemID}`, {
+    const handleModifyCartItemQuantity = async (cartItem, modifyQuantityBy) => {
+        await fetch(`/api/cart/${cartItem._id}`, {
             method: "PATCH",
             headers: {
                 "Content-Type" : "application/json"
             },
             body: JSON.stringify({ modifyQuantityBy })
         }).then((res) => res.json())
-          .then((data) => setCartItems(data.data))
+          .then((data) => {
+              const cartItemsData = data.data
+              setCartItems(cartItemsData)
+          })
     }
 
-    const handleDeleteCartItem = async (cartItemID) => {
-        await fetch(`/api/cart/${cartItemID}`, {
+    const handleDeleteCartItem = async (cartItem) => {
+        await fetch(`/api/cart/${cartItem._id}`, {
             method: "DELETE",
         }).then((res) => res.json())
-          .then((data) => setCartItems(data.data))
+          .then((data) => {
+              const cartItemsData = data.data
+              setCartItems(cartItemsData)
+          })
     }
 
     const handleDeleteAllCartItems = async () => {
         await fetch("/api/cart", {
             method: "DELETE",
         }).then((res) => res.json())
-          .then((data) => setCartItems(data.data))
+          .then((data) => {
+              const cartItemsData = data.data
+              setCartItems(cartItemsData)
+          })
     }
+
+    const doesMenuItemMatchCartItem = (menuItem, cartItem) => {
+        return menuItem._id === cartItem._id
+    }
+
+    const handleFindTotalPriceOfCart = (cartItems, menuItems) => {
+        let totalPrice = 0
+    
+        cartItems.map((cartItem) => {
+          const cartItemQuantity = getCartItemQuantity(cartItem)
+          const menuItem = menuItems.find((menuItem) => doesMenuItemMatchCartItem(menuItem, cartItem))
+          const menuItemPrice = getMenuItemPrice(menuItem)
+          totalPrice += menuItemPrice * cartItemQuantity
+        })
+    
+        return totalPrice.toFixed(2)
+      }
 
     // When the cartItems state changes we run this hook to calculate
     // the total quantity of all items in our cart to display a value for the user
@@ -62,11 +97,7 @@ export const AppProvider = ({ children }) => {
         setCartTotalQuantity(calculatedCartTotalQuantity)
     }, [cartItems])
 
-    // Cart accessor functions
-    const getCartItemID = (cartItem) => {
-        return cartItem._id
-    }
-    
+    // Cart accessor functions    
     const getCartItemQuantity = (cartItem) => {
         return cartItem.quantity
     }
@@ -106,7 +137,8 @@ export const AppProvider = ({ children }) => {
                 handleModifyCartItemQuantity,
                 handleDeleteCartItem,
                 handleDeleteAllCartItems,
-                getCartItemID,
+                doesMenuItemMatchCartItem,
+                handleFindTotalPriceOfCart,
                 getCartItemQuantity,
                 getMenuItemID,
                 getMenuItemName,
