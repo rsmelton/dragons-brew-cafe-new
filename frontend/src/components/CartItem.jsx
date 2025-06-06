0993e8eac42e3b662513417b25e824af716459e5
Robert Melton <rsmelton@crimson.ua.edu>
2025-06-06 03:18:00 -0500

    Now using accessors inside my CartItem component as the component does not need to know about implementation details -- Change made in CartItem.jsx file

--- frontend/src/components/CartItem.jsx
+++ frontend/src/components/CartItem.jsx
@@ -1,75 +1,70 @@
 import React from 'react'
 import { useCart } from '../context/AppContext.jsx'
-import { Flex, Text } from '@chakra-ui/react'
 import '../assets/styles.css'
 
-const CartItem = ({ cartItemID, cartItemQuantity, menuItemName, menuItemPrice }) => {
+const CartItem = ({ cartItem, menuItem }) => {
 
-  const {handleDeleteCartItem, handleModifyCartItemQuantity} = useCart()
+  const {handleDeleteCartItem, handleModifyCartItemQuantity, getCartItemQuantity, 
+         getMenuItemName, getMenuItemPrice} = useCart()
 
   return (
-    // Grid implementation
-    // <Flex
-    //   flexDirection={'column'}
-    //   gap={'0.25rem'}
-    //   paddingTop={'2rem'}
-    //   paddingBottom={'2rem'}
-    // >
-    //   <Flex
-    //     flexDirection={'row'}
-    //     justifyContent={'center'}
-    //     alignItems={'center'}
-    //     gap={'1rem'}
-    //     fontSize={{ base: '1.25rem', sm: '1.5rem' }}
-    //   >
-    //     <Text>{menuItemName}</Text>
-    //     <Text>${menuItemPrice}</Text>
-    //   </Flex>
-    //   <Flex
-    //     flexDirection={'row'}
-    //     justifyContent={'center'}
-    //     alignItems={'center'}
-    //   >
-    //     <div className='cart-item-clicker-container'>
-    //       {/* Here we are decrementing the quantitiy of the cart item */}
-    //       {(cartItemQuantity > 1) && (
-    //         <img className='cart-minus-icon' src={'/images/minus-icon.png'} onClick={() => handleModifyCartItemQuantity(cartItemID, -1)} alt="Minus Icon" />
-    //       )}
-    //       {/* Here we are removing the cartItem all together */}
-    //       {cartItemQuantity === 1 && (
-    //         <img className='trashcan-icon' src={'/images/trashcan-icon.png'} onClick={() => handleDeleteCartItem(cartItemID)} alt="Trashcan Icon" />
-    //       )}
-    //       <div className='cart-item-quantity'>{cartItemQuantity}</div>
-    //       {/* Here we are incrementing the quantitiy of the cart item */}
-    //         <img className='cart-plus-icon' src={'/images/plus-icon.png'} onClick={() => handleModifyCartItemQuantity(cartItemID, 1)} alt="Plus Icon" />
-    //     </div>
-    //   </Flex>
-    // </Flex>
-    // Table implementation
     <tr>
       <td className='cart-table-padding-right'>
         <div className='cart-item-clicker-container'>
           {/* Here we are decrementing the quantitiy of the cart item */}
-          {cartItemQuantity > 1 && (
-            <img className='cart-minus-icon' src={'/images/minus-icon.png'} onClick={() => handleModifyCartItemQuantity(cartItemID, -1)} alt="Minus Icon" />
+          {getCartItemQuantity(cartItem) > 1 && (
+            <img className='cart-minus-icon' src={'/images/minus-icon.png'} onClick={() => handleModifyCartItemQuantity(cartItem, -1)} alt="Minus Icon" />
           )}
           {/* Here we are removing the cartItem all together */}
-          {cartItemQuantity === 1 && (
-            <img className='trashcan-icon' src={'/images/trashcan-icon.png'} onClick={() => handleDeleteCartItem(cartItemID)} alt="Trashcan Icon" />
+          {getCartItemQuantity(cartItem) === 1 && (
+            <img className='trashcan-icon' src={'/images/trashcan-icon.png'} onClick={() => handleDeleteCartItem(cartItem)} alt="Trashcan Icon" />
           )}
-          <div>{cartItemQuantity}</div>
+          <div>{getCartItemQuantity(cartItem)}</div>
           {/* Here we are incrementing the quantitiy of the cart item */}
-          <img className='cart-plus-icon' src={'/images/plus-icon.png'} onClick={() => handleModifyCartItemQuantity(cartItemID, 1)} alt="Plus Icon" />
+          <img className='cart-plus-icon' src={'/images/plus-icon.png'} onClick={() => handleModifyCartItemQuantity(cartItem, 1)} alt="Plus Icon" />
         </div>
       </td>
-      <td className='cart-table-padding-right'>{menuItemName}</td>
-      <td className='cart-table-padding-right' style={{textAlign: 'right'}}>${menuItemPrice.toFixed(2)}</td>
-      <td className='cart-table-padding-right' style={{textAlign: 'right'}}>x</td>
-      <td className='cart-table-padding-right' style={{textAlign: 'right'}}>{cartItemQuantity}</td>
-      <td className='cart-table-padding-right' style={{textAlign: 'right'}}>=</td>
-      <td className='cart-table-padding-right' style={{textAlign: 'right'}}>${(menuItemPrice * cartItemQuantity).toFixed(2)}</td>
+      <td className='cart-table-padding-right'>{getMenuItemName(menuItem)}</td>
+      <td className='cart-table-padding-right' style={{textAlign: 'right'}}>${getMenuItemPrice(menuItem).toFixed(2)}</td>
+      <td style={{textAlign: 'right', paddingRight: '0.5rem'}}>x</td>
+      <td className='cart-table-padding-right' style={{textAlign: 'right'}}>{getCartItemQuantity(cartItem)}</td>
+      <td style={{textAlign: 'right', paddingRight: '0.5rem'}}>=</td>
+      <td className='cart-table-padding-right' style={{textAlign: 'right'}}>${(getMenuItemPrice(menuItem) * getCartItemQuantity(cartItem)).toFixed(2)}</td>
     </tr>
   )
 }
 
 export default CartItem
+
+// const CartItem = ({ cartItemID, cartItemQuantity, menuItemName, menuItemPrice }) => {
+
+//   const {handleDeleteCartItem, handleModifyCartItemQuantity} = useCart()
+
+//   return (
+//     <tr>
+//       <td className='cart-table-padding-right'>
+//         <div className='cart-item-clicker-container'>
+//           {/* Here we are decrementing the quantitiy of the cart item */}
+//           {cartItemQuantity > 1 && (
+//             <img className='cart-minus-icon' src={'/images/minus-icon.png'} onClick={() => handleModifyCartItemQuantity(cartItemID, -1)} alt="Minus Icon" />
+//           )}
+//           {/* Here we are removing the cartItem all together */}
+//           {cartItemQuantity === 1 && (
+//             <img className='trashcan-icon' src={'/images/trashcan-icon.png'} onClick={() => handleDeleteCartItem(cartItemID)} alt="Trashcan Icon" />
+//           )}
+//           <div>{cartItemQuantity}</div>
+//           {/* Here we are incrementing the quantitiy of the cart item */}
+//           <img className='cart-plus-icon' src={'/images/plus-icon.png'} onClick={() => handleModifyCartItemQuantity(cartItemID, 1)} alt="Plus Icon" />
+//         </div>
+//       </td>
+//       <td className='cart-table-padding-right'>{menuItemName}</td>
+//       <td className='cart-table-padding-right' style={{textAlign: 'right'}}>${menuItemPrice.toFixed(2)}</td>
+//       <td className='cart-table-padding-right' style={{textAlign: 'right'}}>x</td>
+//       <td className='cart-table-padding-right' style={{textAlign: 'right'}}>{cartItemQuantity}</td>
+//       <td className='cart-table-padding-right' style={{textAlign: 'right'}}>=</td>
+//       <td className='cart-table-padding-right' style={{textAlign: 'right'}}>${(menuItemPrice * cartItemQuantity).toFixed(2)}</td>
+//     </tr>
+//   )
+// }
+
+// export default CartItem
