ce48dbcf71b92a1ee4bc7972ed985f5528448e49
Robert Melton <rsmelton@crimson.ua.edu>
2025-06-13 23:36:33 -0500

    Removed unneeded Chakra components from ReviewsPage, updated a class name in AboutPage, added 1 class and updated a class in css -- Changed file(s): AboutPage.jsx, ReviewsPage.jsx, styles.css

--- frontend/src/pages/ReviewsPage.jsx
+++ frontend/src/pages/ReviewsPage.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
-import { Box, Text } from '@chakra-ui/react'
+import '../assets/styles.css'
 
 const ReviewsPage = () => {
   return (
-    <Box>
-        <Text>Name: Robert Melton</Text>
-        <Text>Review: ⭐️⭐️⭐️⭐️⭐️</Text>
-        <Text paddingBottom={'1rem'}>Dragon's Brew is my go-to spot for a perfect cup of coffee! The ambiance is cozy with just the right touch of dragon-themed decor that adds a whimsical charm. The baristas are friendly and knowledgeable, always recommending new blends to try. Their Dragon's Fire Latte is my favorite; it's rich and smooth, perfect for starting my day right!</Text>
-        <Text>Name: Deondre North</Text>
-        <Text>Review: ⭐️⭐️⭐️⭐️⭐️</Text>
-        <Text paddingBottom={'1rem'}>What a gem! Dragon's Brew not only serves amazing coffee but also creates a welcoming atmosphere that feels like a home away from home. The moment you step in, you're greeted with the aroma of freshly ground beans and a smile from the staff. I love their attention to detail, from the unique dragon art on the walls to the carefully curated menu. Whether you're in for a quick espresso or a leisurely afternoon latte, this place delivers excellence every time.</Text>
-        <Text>Name: Brandon Jones</Text>
-        <Text>Review: ⭐️⭐️⭐️⭐️⭐️</Text>
-        <Text>Dragon's Brew has quickly become my favorite coffee haunt in town. The quality of their coffee is unmatched, sourced from the best beans and expertly brewed to perfection. It's evident that they care about the craft, as each cup is consistently delicious. Beyond the coffee, the staff here truly makes the experience exceptional. They're not just baristas; they're passionate about coffee and always willing to share their knowledge. Whether you're a coffee aficionado or simply enjoy a good cup, Dragon's Brew is a must-visit!</Text>
-    </Box>
+    <>
+      <p>Name: Robert Melton</p>
+      <p>Review: ⭐️⭐️⭐️⭐️⭐️</p>
+      <p className='review-paragraph-padding-bottom' paddingBottom={'1rem'}>Dragon's Brew is my go-to spot for a perfect cup of coffee! The ambiance is cozy with just the right touch of dragon-themed decor that adds a whimsical charm. The baristas are friendly and knowledgeable, always recommending new blends to try. Their Dragon's Fire Latte is my favorite; it's rich and smooth, perfect for starting my day right!</p>
+      <p>Name: Deondre North</p>
+      <p>Review: ⭐️⭐️⭐️⭐️⭐️</p>
+      <p className='review-paragraph-padding-bottom' paddingBottom={'1rem'}>What a gem! Dragon's Brew not only serves amazing coffee but also creates a welcoming atmosphere that feels like a home away from home. The moment you step in, you're greeted with the aroma of freshly ground beans and a smile from the staff. I love their attention to detail, from the unique dragon art on the walls to the carefully curated menu. Whether you're in for a quick espresso or a leisurely afternoon latte, this place delivers excellence every time.</p>
+      <p>Name: Brandon Jones</p>
+      <p>Review: ⭐️⭐️⭐️⭐️⭐️</p>
+      <p className='review-paragraph-padding-bottom'>Dragon's Brew has quickly become my favorite coffee haunt in town. The quality of their coffee is unmatched, sourced from the best beans and expertly brewed to perfection. It's evident that they care about the craft, as each cup is consistently delicious. Beyond the coffee, the staff here truly makes the experience exceptional. They're not just baristas; they're passionate about coffee and always willing to share their knowledge. Whether you're a coffee aficionado or simply enjoy a good cup, Dragon's Brew is a must-visit!</p>
+    </>
   )
 }
 
